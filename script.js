687256f0a0ba786fb7078749123384a74b52be57
Mayur Ghodaskar <97151293+Mayur244@users.noreply.github.com>
2025-10-30 22:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
   // your code here
+	let n = str.length;
+	let start = 0;
+	let end = size;
+	let arr = [];
+
+	for(let i = 0; i < n/size; i++){
+		let chunk = str.slice(start, end);
+		arr.push(chunk);
+		start += size;
+		end += size;
+	}
+	return arr;
 }
 
 // Do not change the code below
